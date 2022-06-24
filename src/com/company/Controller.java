cd03f6f7bf317e283988c49d3c9b33914e208416
jivot <alkurlaev@yandex.ru>
2022-06-24 15:15:52 +0300

    Added a basic window

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,13 +1,28 @@
 package com.company;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Controller {
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
     private View view;
+    private Graphics graphics;
 
     public void setView(View view) {
         this.view = view;
     }
 
     public void start() {
-        view.create();
+        view.create(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        render();
+    }
+
+    private void render() {
+        BufferedImage image = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        graphics = image.getGraphics();
+
+        view.setImage(image);
     }
 }
--- src/com/company/View.java
+++ src/com/company/View.java
@@ -1,13 +1,37 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
 public class View {
     private Controller controller;
+    private JFrame frame;
+    private JLabel label;
+
+    public void close() {
+        frame.dispose();
+    }
 
     public void setController(Controller controller) {
         this.controller = controller;
     }
 
-    public void create() {
+    public void create(int width, int height) {
+        frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setUndecorated(true);
+        frame.setLocationRelativeTo(null);
 
+        label = new JLabel();
+        label.setBounds(0, 0, width, height);
+        frame.add(label);
+
+        frame.setVisible(true);
+    }
+
+    public void setImage(BufferedImage image) {
+        frame.setIconImage(image);
     }
 }
